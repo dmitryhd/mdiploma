384d73bb943a396ed93dfff0d54a8b1c180989e5
lakri <ghost.lakri@gmail.com;>
2014-05-13 08:18:55 +0400

    exp added

--- diplom_code/experiment.m
+++ diplom_code/experiment.m
@@ -1,5 +1,25 @@
-function perceptron_communicate()
+function experiment()
     % Creates 2 perceptrons. and train them with one another.
+    function out = theta(a, b)
+        if (a ~= b)
+            out = 0;
+        else
+            out = a;
+        end
+    end 
+    function normal_weights = g(weights, L)
+        for i = 1:length(weights)
+            if (weights(i) > L) 
+                weights(i) = L;
+            end
+            if (weights(i) < -L) 
+                weights(i) = -L;
+            end
+        end
+    end
+    function x = get_random_x(input_number)
+        x = randi(5, input_number, 1) - 1;
+    end
     training_set = [[0; 0; 0] [0; 0; 1] [0; 1; 1] [0; 1; 1] [1; 1; 1] [1; 1; 0]];
     training_answers = [0 1 1 1 1 1]; % targets
     perc = perceptron;
@@ -7,10 +27,10 @@ function perceptron_communicate()
     input_config = [0 1; 0 1; 0 1];
     output_config = [0 1];
 
-    perc = configure(perc, input_config, output_config);
+    perc01 = configure(perc, input_config, output_config);
+    perc02 = configure(perc, input_config, output_config);
     ls = [];  % learning state 
-    for epoch = 1:3
-        for tr_index = 1:length(training_set)
+    for tr_index = 1:length(training_set)
             % use trainc Cyclical order weight/bias training
             %{
             w = perc.iw{1,1};
@@ -24,16 +44,19 @@ function perceptron_communicate()
             w = w + dw;
             perc.iw{1,1} = w;
             %}
-            
-            perc = train(perc, training_set(:,tr_index), training_answers(tr_index));
-        end
+        ans01 = perc01(training_set(:,tr_index));
+        ans02 = perc02(training_set(:,tr_index));
+        perc01 = train(perc01, training_set(:,tr_index), training_answers(tr_index));
+        perc02 = train(perc02, training_set(:,tr_index), training_answers(tr_index));
     end
     perceptron_output = zeros(1,length(training_answers));
     % check output
     for exam_index = 1:length(training_set)
-        perceptron_output(exam_index) = perc(training_set(:,exam_index));
+        perceptron_output(exam_index) = perc01(training_set(:,exam_index));
     end
     training_answers
     perceptron_output
+    perc01.iw{1,1}
+    perc02.iw{1,1}
 end
 
